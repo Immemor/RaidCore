4acbd625b1de5f1d22c6e21c0268e785c6bd7e85
NielsH <github@nuvini.com>
2015-01-29 21:22:17 +0100

    Experimental Avatus changes
    Experimental!! You'll prob encounter unexpected things if you use this
    as release.

--- Modules/Avatus.lua
+++ Modules/Avatus.lua
@@ -8,7 +8,12 @@ mod:RegisterEnableMob("Avatus")
 
 local phase2warn, phase2 = false, false
 local phase_blueroom = false
+local phase2_blueroom_rotation = {}
 local encounter_started = false
+local redBuffCount = 1
+local greenBuffCount = 1
+local blueBuffCount = 1
+local buffCountTimer = nil
 local gungrid_time = nil
 -- 40man: first after 46sec - 20m: first after 20sec, after that every 112 sec
 local gungrid_timer = 46
@@ -27,9 +32,10 @@ function mod:OnBossEnable()
 	Apollo.RegisterEventHandler("CHAT_DATACHRON", 		"OnChatDC", self)
 	--Apollo.RegisterEventHandler("RAID_SYNC", 			"OnSyncRcv", self)
 	--Apollo.RegisterEventHandler("SubZoneChanged", 	"OnZoneChanged", self)
-	--Apollo.RegisterEventHandler("CHAT_NPCSAY", 		"OnChatNPCSay", self)
+	Apollo.RegisterEventHandler("CHAT_NPCSAY", 			"OnChatNPCSay", self)
 	Apollo.RegisterEventHandler("RAID_WIPE", 			"OnReset", self)
 	Apollo.RegisterEventHandler("BUFF_APPLIED", 		"OnBuffApplied", self)
+	Apollo.RegisterEventHandler("ChatMessage", 			"OnChatMessage", self)
 end
 
 local function dist2unit(unitSource, unitTarget)
@@ -45,9 +51,36 @@ local function dist2unit(unitSource, unitTarget)
 	return tonumber(dist)
 end
 
+local function lowestKeyValue(tbl)
+	local lowestValue = false
+	local tmp_table = {}
+	for key, value in pairs(tbl) do
+		if not lowestValue or (value < lowestValue) then
+			lowestValue = value
+			tmp_table = {}
+			tmp_table[key] = value
+		elseif value == lowestValue then
+		    tmp_table[key] = value
+		end
+    end
+    -- Now that we only have the keys with the lowest values left, order alphabetically
+    local lowest_key = nil
+    for key, value in pairs(tmp_table) do
+        if not lowest_key or (key < lowest_key) then
+            lowest_key = key
+        end
+    end
+    return lowest_key
+end
+
 function mod:OnReset()
 	phase2warn, phase2 = false, false
 	phase_blueroom = false
+	phase2_blueroom_rotation = {}
+	redBuffCount = 1
+	greenBuffCount = 1
+	blueBuffCount = 1
+	buffCountTimer = nil
 	encounter_started = false
 	gungrid_timer = nil
 	gungrid_timer = 46
@@ -94,7 +127,7 @@ end
 
 function mod:OnBuffApplied(unitName, splId, unit)
 	local eventTime = GameLib.GetGameTime()
-	--[[if phase2_blueroom and unitName == "Infinite Logic Loop" then
+	if phase2_blueroom and unitName == "Infinite Logic Loop" then
 		local tSpell = GameLib.GetSpell(splId)
 		local strSpellName
 		if tSpell then
@@ -105,18 +138,25 @@ function mod:OnBuffApplied(unitName, splId, unit)
 
 		-- Todo change to SplId instead of name to reduce API calls
 		if strSpellName == "Green Reconstitution Matrix" then
-			Print(eventTime .. " " .. unitName .. " has the GREEN buff")
-			ChatSystemLib.Command('/p ' .. unitName .. " has GREEN buff")
+			local playerAssigned = lowestKeyValue(phase2_blueroom_rotation["green"])
+			ChatSystemLib.Command('/p [#' .. tostring(greenBuffCount) .. '] ' .. unitName .. " has GREEN buff - assigned to: " .. playerAssigned)
+			greenBuffCount = greenBuffCount + 1
+			phase2_blueroom_rotation["green"][playerAssigned] = phase2_blueroom_rotation["green"][playerAssigned] + 1
+			if not buffCountTimer then self:ScheduleTimer("ResetBuffCount", 13) end
 		elseif strSpellName == "Blue Disruption Matrix" then
-			Print(eventTime .. " " .. unitName .. " has the BLUE buff")
-			ChatSystemLib.Command('/p ' .. unitName .. " has BLUE buff")
+			local playerAssigned = lowestKeyValue(phase2_blueroom_rotation["blue"])
+			ChatSystemLib.Command('/p [#' .. tostring(blueBuffCount) .. '] ' .. unitName .. " has BLUE buff - assigned to: " .. playerAssigned)
+			phase2_blueroom_rotation["blue"][playerAssigned] = phase2_blueroom_rotation["blue"][playerAssigned] + 1
+			blueBuffCount = blueBuffCount + 1
+			if not buffCountTimer then self:ScheduleTimer("ResetBuffCount", 13) end
 		elseif strSpellName == "Red Empowerment Matrix" then
-			Print(eventTime .. " " .. unitName .. " has the RED buff")
-			ChatSystemLib.Command('/p ' .. unitName .. " has RED buff")
+			local playerAssigned = lowestKeyValue(phase2_blueroom_rotation["red"])
+			ChatSystemLib.Command('/p [#' .. tostring(redBuffCount) .. '] ' .. unitName .. " has RED buff - assigned to: " .. playerAssigned)
+			phase2_blueroom_rotation["red"][playerAssigned] = phase2_blueroom_rotation["red"][playerAssigned] + 1
+			redBuffCount = redBuffCount + 1
+			if not buffCountTimer then self:ScheduleTimer("ResetBuffCount", 13) end
 		end		
-
-		Print(eventTime .. " " .. unitName .. " has a buff: " .. strSpellName .. " with SplId: " .. splId)
-	end--]]
+	end
 end
 
 function mod:OnHealthChanged(unitName, health)
@@ -176,6 +216,32 @@ function mod:OnChatDC(message)
 	end
 end
 
+function mod:OnChatMessage(channelCurrent, tMessage)
+	local strChannelName = channelCurrent:GetName()
+	if strChannelName == "Party" and phase2 then
+		local msg = string.lower(tMessage["arMessageSegments"][1]["strText"])
+		local strSender = tMessage["strSender"]
+		
+		if msg == "red" then
+			if not phase2_blueroom_rotation["red"] then phase2_blueroom_rotation["red"] = {} end
+			phase2_blueroom_rotation["red"][strSender] = 1
+		elseif msg == "green" then
+			if not phase2_blueroom_rotation["green"] then phase2_blueroom_rotation["green"] = {} end
+			phase2_blueroom_rotation["green"][strSender] = 1
+		elseif msg == "blue" then
+			if not phase2_blueroom_rotation["blue"] then phase2_blueroom_rotation["blue"] = {} end
+			phase2_blueroom_rotation["blue"][strSender] = 1
+		end
+	end
+end
+
+function mod:ResetBuffCount()
+	redBuffCount = 1
+	greenBuffCount = 1
+	blueBuffCount = 1
+	buffCountTimer = nil
+end
+
 function mod:OnCombatStateChanged(unit, bInCombat)
 	if unit:GetType() == "NonPlayer" and bInCombat then
 		local sName = unit:GetName()
@@ -186,6 +252,11 @@ function mod:OnCombatStateChanged(unit, bInCombat)
 			encounter_started = true
 			phase2warn, phase2 = false, false
 			phase2_blueroom = false
+			phase2_blueroom_rotation = {}
+			redBuffCount = 1
+			greenBuffCount = 1
+			blueBuffCount = 1
+			buffCountTimer = nil
 			gungrid_time = eventTime + gungrid_timer
 			holo_hands = {}
 			core:AddUnit(unit)
@@ -198,6 +269,34 @@ function mod:OnCombatStateChanged(unit, bInCombat)
 			core:AddBar("GGRID", "Gun Grid", gungrid_timer, true)
 			gungrid_timer = 112
 			obliteration_beam_timer = 37
+		elseif sName == "Infinite Logic Loop" then
+			local strRedBuffs = "Red Buffs:"
+			local strGreenBuffs = "Green Buffs:"
+			local strBlueBuffs = "Blue Buffs:"
+		
+			local redPlayerCount = 1
+			local greenPlayerCount = 1
+			local bluePlayerCount = 1
+
+			for key, value in pairs(phase2_blueroom_rotation["red"]) do
+				strRedBuffs = strRedBuffs .. " - " .. tostring(redPlayerCount) .. ". " .. key
+				redPlayerCount = redPlayerCount + 1
+			end
+			for key, value in pairs(phase2_blueroom_rotation["green"]) do
+				strGreenBuffs = strGreenBuffs .. " - " .. tostring(greenPlayerCount) .. ". " .. key
+				greenPlayerCount = greenPlayerCount + 1
+			end
+			for key, value in pairs(phase2_blueroom_rotation["blue"]) do
+				strBlueBuffs = strBlueBuffs .. " - " .. tostring(bluePlayerCount) .. ". " .. key
+				bluePlayerCount = bluePlayerCount + 1
+			end
+
+			ChatSystemLib.Command('/p ' .. strRedBuffs)
+			ChatSystemLib.Command('/p ' .. strGreenBuffs)
+			ChatSystemLib.Command('/p ' .. strBlueBuffs)
+
+			local Rover = Apollo.GetAddon("Rover")
+			Rover:AddWatch("phase2rotation", phase2_blueroom_rotation, 0)
 		end
 	end
 end
