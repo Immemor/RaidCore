eb25c08bcb359db3067a2d4c5dc5fc4c1b1f4f8d
Immé sama <immemor@users.noreply.github.com>
2015-04-04 23:41:21 +0200

    Modules: Add locale for PhageCouncil

--- Modules/PhageCouncil.lua
+++ Modules/PhageCouncil.lua
@@ -10,6 +10,49 @@ if not mod then return end
 mod:RegisterEnableMob("Golgox the Lifecrusher", "Terax Blightweaver", "Ersoth Curseform", "Noxmind the Insidious", "Fleshmonger Vratorg")
 mod:RegisterRestrictZone("PhageCouncil", "Augmentation Core")
 mod:RegisterEnableZone("PhageCouncil", "Augmentation Core")
+mod:RegisterEnglishLocale({
+	-- Unit names.
+	["Terax Blightweaver"] = "Terax Blightweaver",
+	["Golgox the Lifecrusher"] = "Golgox the Lifecrusher",
+	["Fleshmonger Vratorg"] = "Fleshmonger Vratorg",
+	["Noxmind the Insidious"] = "Noxmind the Insidious",
+	["Ersoth Curseform"] = "Ersoth Curseform",
+	-- Datachron messages.
+	["The Phageborn Convergence begins gathering its power"] = "The Phageborn Convergence begins gathering its power",
+	-- Cast.
+	["Teleport"] = "Teleport",
+	["Channeling Energy"] = "Channeling Energy",
+	-- Bar and messages.
+	["[%u] NEXT P2"] = "[%u] NEXT P2",
+	["P2 : 20 IA"] = "P2 : 20 IA",
+	["P2 : MINI ADDS"] = "P2 : MINI ADDS",
+	["P2 : SUBDUE"] = "P2 : SUBDUE",
+	["P2 : PILLARS"] = "P2 : PILLARS",
+})
+mod:RegisterFrenchLocale({
+	-- Unit names.
+	["Terax Blightweaver"] = "Terax Tisserouille",
+	["Golgox the Lifecrusher"] = "Golgox le Fossoyeur",
+	["Fleshmonger Vratorg"] = "Vratorg le Cannibale",
+	["Noxmind the Insidious"] = "Toxultime l'Insidieux",
+	["Ersoth Curseform"] = "Ersoth le Maudisseur",
+	-- Datachron messages.
+	-- Cast.
+	["Teleport"] = "Se téléporter",
+	-- Bar and messages.
+	["[%u] NEXT P2"] = "[%u] PROCHAINE P2",
+	["P2 : 20 IA"] = "P2 : 20 IA",
+	["P2 : MINI ADDS"] = "P2 : MINI ADDS",
+	["P2 : SUBDUE"] = "P2 : DESARMEMENT",
+	["P2 : PILLARS"] = "P2 : PILLIERS",
+})
+mod:RegisterGermanLocale({
+	-- Unit names.
+	-- Datachron messages.
+	-- Cast.
+	-- Bar and messages.
+})
+
 
 --------------------------------------------------------------------------------
 -- Locals
@@ -23,7 +66,7 @@ local p2Count = 0
 
 function mod:OnBossEnable()
 	Print(("Module %s loaded"):format(mod.ModuleName))
-	Apollo.RegisterEventHandler("UnitEnteredCombat", "OnCombatStateChanged", self)
+	Apollo.RegisterEventHandler("RC_UnitStateChanged", "OnUnitStateChanged", self)
 	Apollo.RegisterEventHandler("SPELL_CAST_START", "OnSpellCastStart", self)
 	Apollo.RegisterEventHandler("SPELL_CAST_END", "OnSpellCastEnd", self)
 	Apollo.RegisterEventHandler("CHAT_DATACHRON", "OnChatDC", self)
@@ -34,46 +77,49 @@ end
 --
 
 function mod:OnSpellCastStart(unitName, castName, unit)
-	if unitName == "Golgox the Lifecrusher" and castName == "Teleport" then
-		core:AddMsg("CONVP2", "P2 : 20 IA", 5, "Alert")
-		core:AddBar("CONVP2", "P2 : 20 IA", 29.5)
-	elseif unitName == "Terax Blightweaver" and castName == "Teleport" then
-		core:AddMsg("CONVP2", "P2 : MINI ADDS", 5, "Alert")
-		core:AddBar("CONVP2", "P2 : MINI ADDS", 29.5)
-	elseif unitName == "Ersoth Curseform" and castName == "Teleport" then
-		core:AddMsg("CONVP2", "P2 : SUBDUE", 5, "Alert")
-		core:AddBar("CONVP2", "P2 : SUBDUE", 29.5)
-	elseif unitName == "Noxmind the Insidious" and castName == "Teleport" then
-		core:AddMsg("CONVP2", "P2 : PILLARS", 5, "Alert")
-		core:AddBar("CONVP2", "P2 : PILLARS", 29.5)
-	elseif unitName == "Fleshmonger Vratorg" and castName == "Teleport" then
-		core:AddMsg("CONVP2", "P2 : SHIELD", 5, "Alert")
-		core:AddBar("CONVP2", "P2 : SHIELD", 29.5)
+	if unitName == self.L["Golgox the Lifecrusher"] and castName == self.L["Teleport"] then
+		core:AddMsg("CONVP2", self.L["P2 : 20 IA"], 5, "Alert")
+		core:AddBar("CONVP2", self.L["P2 : 20 IA"], 29.5)
+	elseif unitName == self.L["Terax Blightweaver"] and castName == self.L["Teleport"] then
+		core:AddMsg("CONVP2", self.L["P2 : MINI ADDS"], 5, "Alert")
+		core:AddBar("CONVP2", self.L["P2 : MINI ADDS"], 29.5)
+	elseif unitName == self.L["Ersoth Curseform"] and castName == self.L["Teleport"] then
+		core:AddMsg("CONVP2", self.L["P2 : SUBDUE"], 5, "Alert")
+		core:AddBar("CONVP2", self.L["P2 : SUBDUE"], 29.5)
+	elseif unitName == self.L["Noxmind the Insidious"] and castName == self.L["Teleport"] then
+		core:AddMsg("CONVP2", self.L["P2 : PILLARS"], 5, "Alert")
+		core:AddBar("CONVP2", self.L["P2 : PILLARS"], 29.5)
+	elseif unitName == self.L["Fleshmonger Vratorg"] and castName == self.L["Teleport"] then
+		core:AddMsg("CONVP2", self.L["P2 : SHIELD"], 5, "Alert")
+		core:AddBar("CONVP2", self.L["P2 : SHIELD"], 29.5)
 	end
 end
 
 function mod:OnSpellCastEnd(unitName, castName)
-	if castName == "Channeling Energy" then
+	if castName == self.L["Channeling Energy"] then
 		core:StopScan()
 		core:StopBar("CONVP2")
-		core:AddBar("CONVP1", ("[%s] NEXT P2"):format(p2Count + 1), 60, 1)
+		core:AddBar("CONVP1", self.L["[%u] NEXT P2"]:format(p2Count + 1), 60, 1)
 	end
 end
 
 function mod:OnChatDC(message)
-	if message:find("The Phageborn Convergence begins gathering its power") then
+	if message:find(self.L["The Phageborn Convergence begins gathering its power"]) then
 		p2Count = p2Count + 1
 		core:StartScan()
 	end
 end
 
-function mod:OnCombatStateChanged(unit, bInCombat)
+function mod:OnUnitStateChanged(unit, bInCombat, sName)
 	if unit:GetType() == "NonPlayer" and bInCombat then
-		local sName = unit:GetName()
-		if sName == "Golgox the Lifecrusher" or sName == "Terax Blightweaver"  or sName == "Ersoth Curseform" or sName == "Noxmind the Insidious" or sName == "Fleshmonger Vratorg" then
+		if sName == self.L["Golgox the Lifecrusher"]
+			or sName == self.L["Terax Blightweaver"]
+			or sName == self.L["Ersoth Curseform"]
+			or sName == self.L["Noxmind the Insidious"]
+			or sName == self.L["Fleshmonger Vratorg"] then
 			self:Start()
 			p2Count = 0
-			core:AddBar("CONVP1", ("[%s] NEXT P2"):format(p2Count + 1), 90, 1)
+			core:AddBar("CONVP1", self.L["[%u] NEXT P2"]:format(p2Count + 1), 90, 1)
 			core:AddUnit(unit)
 			core:WatchUnit(unit)
 		end
