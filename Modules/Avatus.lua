cce7a31932f9e39cba300a8ed70098213f831e6b
Immé sama <immemor@users.noreply.github.com>
2015-04-06 13:53:49 +0200

    Modules: Add locale for Avatus

--- Modules/Avatus.lua
+++ Modules/Avatus.lua
@@ -5,6 +5,50 @@ if not mod then return end
 
 mod:RegisterEnableMob("Avatus")
 mod:RegisterRestrictZone("Avatus", "The Oculus")
+mod:RegisterEnglishLocale({
+	-- Unit names.
+	["Avatus"] = "Avatus",
+	["Holo Hand"] = "Holo Hand",
+	["Holo Hand Spawned"] = "Holo Hand Spawned",
+	["Mobius Physics Constructor"] = "Mobius Physics Constructor",
+	["Unstoppable Object Simulation"] = "Unstoppable Object Simulation",
+	["Holo Cannon"] = "Holo Cannon",
+	["Shock Sphere"] = "Shock Sphere",
+	["Support Cannon"] = "Support Cannon",
+	["Infinite Logic Loop"] = "Infinite Logic Loop",
+	-- Datachron messages.
+	["Portals have opened!"] = "Portals have opened!",
+	["Gun Grid Activated"] = "Gun Grid Activated",
+	-- Cast.
+	["Crushing Blow"] = "Crushing Blow",
+	["Data Flare"] = "Data Flare",
+	["Obliteration Beam"] = "Obliteration Beam",
+	-- Bar and messages.
+	["PURGE BLUE BOSS"] = "PURGE BLUE BOSS",
+	["P2 SOON !"] = "P2 SOON !",
+	["GO TO SIDES !"] = "GO TO SIDES !",
+	["INTERRUPT CRUSHING BLOW!"] = "INTERRUPT CRUSHING BLOW!",
+	["BLIND! TURN AWAY FROM BOSS"] = "BLIND! TURN AWAY FROM BOSS",
+	["Blind"] = "Blind",
+	["Gun Grid NOW!"] = "Gun Grid NOW!",
+	["~Gun Grid"] = "~Gun Grid",
+	["Holo Hands spawn"] = "Holo Hands spawn",
+	["Hand %u"] = "Hand %u",
+	["MARKER North"] = "North",
+	["MARKER South"] = "South",
+})
+mod:RegisterFrenchLocale({
+	-- Unit names.
+	-- Datachron messages.
+	-- Cast.
+	-- Bar and messages.
+})
+mod:RegisterGermanLocale({
+	-- Unit names.
+	-- Datachron messages.
+	-- Cast.
+	-- Bar and messages.
+})
 
 local phase2warn, phase2 = false, false
 local phase_blueroom = false
@@ -19,6 +63,7 @@ local gungrid_timer = 20
 local obliteration_beam_timer = 69
 local holo_hands = {}
 local strMyName
+local NO_BREAK_SPACE = string.char(194, 160)
 
 local handpos = {
 	["hand1"] = {x = 608.70, y = -198.75, z = -191.62},
@@ -32,9 +77,9 @@ local referencePos = {
 
 function mod:OnBossEnable()
 	Print(("Module %s loaded"):format(mod.ModuleName))
-	Apollo.RegisterEventHandler("UnitCreated", "OnUnitCreated", self)
-	Apollo.RegisterEventHandler("UnitDestroyed", "OnUnitDestroyed", self)
-	Apollo.RegisterEventHandler("UnitEnteredCombat", "OnCombatStateChanged", self)
+	Apollo.RegisterEventHandler("RC_UnitCreated", "OnUnitCreated", self)
+	Apollo.RegisterEventHandler("RC_UnitDestroyed", "OnUnitDestroyed", self)
+	Apollo.RegisterEventHandler("RC_UnitStateChanged", "OnUnitStateChanged", self)
 	Apollo.RegisterEventHandler("SPELL_CAST_START", "OnSpellCastStart", self)
 	--Apollo.RegisterEventHandler("SPELL_CAST_END", "OnSpellCastEnd", self)
 	Apollo.RegisterEventHandler("UNIT_HEALTH", "OnHealthChanged", self)
@@ -170,29 +215,27 @@ function mod:OnReset()
 	core:ResetWorldMarkers()
 end
 
-function mod:OnUnitCreated(unit)
+function mod:OnUnitCreated(unit, sName)
 	local eventTime = GameLib.GetGameTime()
-
-	local sName = unit:GetName()
 	--Print(eventTime .. " " .. sName .. " spawned")
-	if sName == "Avatus" then
+	if sName == self.L["Avatus"] then
 		core:AddUnit(unit)
 		core:WatchUnit(unit)
 		if GetSetting("LineCleaveBoss") then
 			core:AddPixie(unit:GetId(), 2, unit, nil, "Green", 10, 22, 0)
 		end
-	elseif sName == "Holo Hand" then
+	elseif sName == self.L["Holo Hand"] then
 		--Print(eventTime .. " Holo hand Spawned")
 		local unitId = unit:GetId()
 		core:AddUnit(unit)
 		core:WatchUnit(unit)
 		table.insert(holo_hands, unitId, {["unit"] = unit})
-		core:AddMsg("HHAND", "Holo Hand Spawned", 5, "Info")
+		core:AddMsg("HHAND", self.L["Holo Hand Spawned"], 5, "Info")
 		if unitId and GetSetting("LineCleaveHands") then
 			core:AddPixie(unitId .. "_1", 2, unit, nil, "Blue", 7, 20, 0)
 			--core:AddPixie(unitId .. "_2", 2, unit, nil, "Blue", 7, 20, 270)
 		end
-	elseif sName == "Mobius Physics Constructor" then -- yellow room
+	elseif sName == self.L["Mobius Physics Constructor"] then -- yellow room
 		core:AddUnit(unit)
 		core:WatchUnit(unit)
 		local unitId = unit:GetId()
@@ -201,47 +244,42 @@ function mod:OnUnitCreated(unit)
 				core:AddPixie(unitId, 2, unit, nil, "Red", 5, 35, 0)
 			end
 		end
-	elseif sName == "Unstoppable Object Simulation" then -- green
+	elseif sName == self.L["Unstoppable Object Simulation"] then --green
 		core:AddUnit(unit)
-	elseif sName == "Holo Cannon" and GetSetting("LineCannons") then
+	elseif sName == self.L["Holo Cannon"] and GetSetting("LineCannons") then
 		core:AddPixie(unit:GetId(), 2, unit, nil, "Blue", 5, 100, 0)
-	elseif sName == "Shock Sphere" and GetSetting("LineOrbsYellowRoom") then -- yellow room orbs
+	elseif sName == self.L["Shock Sphere"] and GetSetting("LineOrbsYellowRoom") then -- yellow room orbs
 		core:AddPixie(unit:GetId(), 2, unit, nil, "Blue", 5, -7, 0)
-	elseif sName == "Support Cannon" then
+	elseif sName == self.L["Support Cannon"] then
 		core:AddUnit(unit)
-	end
-
-	-- TESTING BLUE ROOM:
-	if sName == "Infinite Logic Loop" then -- blue
+	elseif sName == self.L["Infinite Logic Loop"] then -- blue
+		-- TESTING BLUE ROOM:
 		core:AddUnit(unit)
 		core:UnitBuff(unit)
 		phase2_blueroom = true
 	end
-
 end
 
-function mod:OnUnitDestroyed(unit)
-	local sName = unit:GetName()
+function mod:OnUnitDestroyed(unit, sName)
 	local unitId = unit:GetId()
 
-	if sName == "Holo Hand" then
+	if sName == self.L["Holo Hand"] then
 		if unitId then
 			core:DropPixie(unitId .. "_1")
 			--core:DropPixie(unitId .. "_2")
 		end
-	end
-
-	if sName == "Holo Hand" and holo_hands[unitId] then
-		holo_hands[unitId] = nil
-	elseif sName == "Holo Cannon" then
+		if holo_hands[unitId] then
+			holo_hands[unitId] = nil
+		end
+	elseif sName == self.L["Holo Cannon"] then
 		core:DropPixie(unit:GetId())
-	elseif sName == "Avatus" then
+	elseif sName == self.L["Avatus"] then
 		core:DropPixie(unit:GetId())
-	elseif sName == "Shock Sphere" then
+	elseif sName == self.L["Shock Sphere"] then
 		core:DropPixie(unit:GetId())
-	elseif sName == "Infinite Logic Loop" then
+	elseif sName == self.L["Infinite Logic Loop"] then
 		phase2_blueroom = false
-	elseif sName == "Mobius Physics Constructor" then
+	elseif sName == self.L["Mobius Physics Constructor"] then
 		core:DropPixie(unit:GetId())
 	end
 	--[[
@@ -253,7 +291,7 @@ end
 
 function mod:OnBuffApplied(unitName, splId, unit)
 	local eventTime = GameLib.GetGameTime()
-	if phase2_blueroom and unitName == "Infinite Logic Loop" then
+	if phase2_blueroom and unitName == self.L["Infinite Logic Loop"] then
 		local tSpell = GameLib.GetSpell(splId)
 		local strSpellName
 		if tSpell then
@@ -266,7 +304,7 @@ function mod:OnBuffApplied(unitName, splId, unit)
 		if strSpellName == "Green Reconstitution Matrix" then
 			local playerAssigned = getPlayerAssignment(phase2_blueroom_rotation["green"])
 			if playerAssigned == strMyName then
-				core:AddMsg("BLUEPURGE", "PURGE BLUE BOSS", 5, GetSoundSetting("Inferno", "SoundBlueInterrupt"))
+				core:AddMsg("BLUEPURGE", self.L["PURGE BLUE BOSS"], 5, GetSoundSetting("Inferno", "SoundBlueInterrupt"))
 			end
 			--ChatSystemLib.Command('/p [#' .. tostring(greenBuffCount) .. '] ' .. unitName .. " has GREEN buff - assigned to: " .. playerAssigned)
 			Print('[#' .. tostring(greenBuffCount) .. '] ' .. unitName .. " has GREEN buff - assigned to: " .. playerAssigned)
@@ -276,7 +314,7 @@ function mod:OnBuffApplied(unitName, splId, unit)
 		elseif strSpellName == "Blue Disruption Matrix" then
 			local playerAssigned = getPlayerAssignment(phase2_blueroom_rotation["blue"])
 			if playerAssigned == strMyName then
-				core:AddMsg("BLUEPURGE", "PURGE BLUE BOSS", 5, GetSoundSetting("Inferno", "SoundBlueInterrupt"))
+				core:AddMsg("BLUEPURGE", self.L["PURGE BLUE BOSS"], 5, GetSoundSetting("Inferno", "SoundBlueInterrupt"))
 			end
 			--ChatSystemLib.Command('/p [#' .. tostring(blueBuffCount) .. '] ' .. unitName .. " has BLUE buff - assigned to: " .. playerAssigned)
 			Print('[#' .. tostring(blueBuffCount) .. '] ' .. unitName .. " has BLUE buff - assigned to: " .. playerAssigned)
@@ -286,7 +324,7 @@ function mod:OnBuffApplied(unitName, splId, unit)
 		elseif strSpellName == "Red Empowerment Matrix" then
 			local playerAssigned = getPlayerAssignment(phase2_blueroom_rotation["red"])
 			if playerAssigned == strMyName then
-				core:AddMsg("BLUEPURGE", "PURGE BLUE BOSS", 5, GetSoundSetting("Inferno", "SoundBlueInterrupt"))
+				core:AddMsg("BLUEPURGE", self.L["PURGE BLUE BOSS"], 5, GetSoundSetting("Inferno", "SoundBlueInterrupt"))
 			end
 			--ChatSystemLib.Command('/p [#' .. tostring(redBuffCount) .. '] ' .. unitName .. " has RED buff - assigned to: " .. playerAssigned)
 			Print('[#' .. tostring(redBuffCount) .. '] ' .. unitName .. " has RED buff - assigned to: " .. playerAssigned)
@@ -298,28 +336,29 @@ function mod:OnBuffApplied(unitName, splId, unit)
 end
 
 function mod:OnHealthChanged(unitName, health)
-	if unitName == "Avatus" and health >= 75 and health <= 77 and not phase2warn then
-		phase2warn = true
-		core:AddMsg("AVAP2", "P2 SOON !", 5, GetSoundSetting("Info", "SoundPortalPhase"))
-	elseif unitName == "Avatus" and health >= 50 and health <= 52 and not phase2warn then
-		phase2warn = true
-		core:AddMsg("AVAP2", "P2 SOON!", 5, GetSoundSetting("Info", "SoundPortalPhase"))
-	end
-	if unitName == "Avatus" and health >= 70 and health <= 72 and phase2warn then
-		phase2warn = false
+	if unitName == self.L["Avatus"] then
+		if health >= 75 and health <= 77 and not phase2warn then
+			phase2warn = true
+			core:AddMsg("AVAP2", self.L["P2 SOON !"], 5, GetSoundSetting("Info", "SoundPortalPhase"))
+		elseif health >= 50 and health <= 52 and not phase2warn then
+			phase2warn = true
+			core:AddMsg("AVAP2", self.L["P2 SOON!"], 5, GetSoundSetting("Info", "SoundPortalPhase"))
+		elseif health >= 70 and health <= 72 and phase2warn then
+			phase2warn = false
+		end
 	end
 end
 
 function mod:OnSpellCastStart(unitName, castName, unit)
 	local eventTime = GameLib.GetGameTime()
-	if unitName == "Avatus" and castName == "Obliteration Beam" then
-		core:AddMsg("BEAMS", "GO TO SIDES !", 5, GetSoundSetting("RunAway", "SoundObliterationBeam"))
+	if unitName == self.L["Avatus"] and castName == self.L["Obliteration Beam"] then
+		core:AddMsg("BEAMS", self.L["GO TO SIDES !"], 5, GetSoundSetting("RunAway", "SoundObliterationBeam"))
 		core:StopBar("OBBEAM")
 		-- check if next ob beam in {obliteration_beam_timer} sec doesn't happen during a gungrid which takes 20 sec
 		if gungrid_time + gungrid_timer + 20 < eventTime + obliteration_beam_timer then
-			core:AddBar("OBBEAM", "Obliteration Beam", obliteration_beam_timer, GetSoundSetting(true, "SoundObliterationBeam"))
+			core:AddBar("OBBEAM", self.L["Obliteration Beam"], obliteration_beam_timer, GetSoundSetting(true, "SoundObliterationBeam"))
 		end
-	elseif unitName == "Holo Hand" and castName == "Crushing Blow" then
+	elseif unitName == self.L["Holo Hand"] and castName == self.L["Crushing Blow"] then
 		local playerUnit = GameLib.GetPlayerUnit()
 		for _, hand in pairs(holo_hands) do
 			local distance_to_hand = dist2unit(playerUnit, hand["unit"])
@@ -332,30 +371,29 @@ function mod:OnSpellCastStart(unitName, castName, unit)
 				closest_holo_hand = hand
 			end
 		end
-		if closest_holo_hand["unit"]:GetCastName() == "Crushing Blow" then
-			core:AddMsg("CRBLOW", "INTERRUPT CRUSHING BLOW!", 5, GetSoundSetting("Inferno", "SoundHandInterrupt"))
+		local sSpellName = closest_holo_hand["unit"]:GetCastName():gsub(NO_BREAK_SPACE, " ")
+		if sSpellName == self.L["Crushing Blow"] then
+			core:AddMsg("CRBLOW", self.L["INTERRUPT CRUSHING BLOW!"], 5, GetSoundSetting("Inferno", "SoundHandInterrupt"))
 		end
-	elseif unitName == "Mobius Physics Constructor" and castName == "Data Flare" then
-		core:AddBar("BLIND", "Blind", 29, GetSoundSetting(true, "SoundBlindYellowRoom"))
-		core:AddMsg("BLIND", "BLIND! TURN AWAY FROM BOSS", 5, GetSoundSetting("Inferno", "SoundBlindYellowRoom"))
+	elseif unitName == self.L["Mobius Physics Constructor"] and castName == self.L["Data Flare"] then
+		core:AddBar("BLIND", self.L["Blind"], 29, GetSoundSetting(true, "SoundBlindYellowRoom"))
+		core:AddMsg("BLIND", self.L["BLIND! TURN AWAY FROM BOSS"], 5, GetSoundSetting("Inferno", "SoundBlindYellowRoom"))
 	end
-
-	--Print(eventTime .. " " .. unitName .. " is casting " .. castName)
 end
 
 function mod:OnChatDC(message)
 	local eventTime = GameLib.GetGameTime()
 	--Print(eventTime .. " ChatDC Message: " .. message)
-	if message:find("Gun Grid Activated") then
+	if message:find(self.L["Gun Grid Activated"]) then
 		--Print(eventTime .. " ChatDC Message: " .. message)
 		gungrid_time = eventTime
-		core:AddMsg("GGRIDMSG", "Gun Grid NOW!", 5, GetSoundSetting("Beware", "SoundGunGrid"))
+		core:AddMsg("GGRIDMSG", self.L["Gun Grid NOW!"], 5, GetSoundSetting("Beware", "SoundGunGrid"))
 		core:StopBar("GGRID")
 		core:StopBar("HHAND")
-		core:AddBar("GGRID", "~Gun Grid", gungrid_timer, GetSoundSetting(true, "SoundGunGrid"))
-		core:AddBar("HHAND", "Holo Hands spawn", 22)
+		core:AddBar("GGRID", self.L["~Gun Grid"], gungrid_timer, GetSoundSetting(true, "SoundGunGrid"))
+		core:AddBar("HHAND", self.L["Holo Hands spawn"], 22)
 	end
-	if message:find("Portals have opened!") then
+	if message:find(self.L["Portals have opened!"]) then
 		phase2 = true
 		core:StopBar("GGRID")
 		core:StopBar("OBBEAM")
@@ -366,8 +404,8 @@ end
 function mod:OnChatMessage(channelCurrent, tMessage)
 	local strChannelName = channelCurrent:GetName()
 	if strChannelName == "Party" and phase2 then
-		local msg = string.lower(tMessage["arMessageSegments"][1]["strText"])
-		local strSender = tMessage["strSender"]
+		local msg = tMessage.arMessageSegments[1].strText:lower()
+		local strSender = tMessage["strSender"]:gsub(NO_BREAK_SPACE, " ")
 
 		if msg == "red" then
 			if not phase2_blueroom_rotation["red"] then phase2_blueroom_rotation["red"] = {} end
@@ -389,11 +427,9 @@ function mod:ResetBuffCount()
 	buffCountTimer = false
 end
 
-function mod:OnCombatStateChanged(unit, bInCombat)
+function mod:OnUnitStateChanged(unit, bInCombat, sName)
 	if unit:GetType() == "NonPlayer" and bInCombat then
-		local sName = unit:GetName()
-
-		if sName == "Avatus" and not encounter_started then
+		if sName == self.L["Avatus"] and not encounter_started then
 			local eventTime = GameLib.GetGameTime()
 			self:Start()
 			encounter_started = true
@@ -411,21 +447,19 @@ function mod:OnCombatStateChanged(unit, bInCombat)
 			core:WatchUnit(unit)
 			core:StartScan()
 
-			--Print(eventTime .. " " .. sName .. " FIGHT STARTED ")
-
-			core:AddBar("OBBEAM", "Obliteration Beam", obliteration_beam_timer, GetSoundSetting(true, "SoundObliterationBeam"))
-			core:AddBar("GGRID", "~Gun Grid", gungrid_timer, GetSoundSetting(true, "SoundGunGrid"))
+			core:AddBar("OBBEAM", self.L["Obliteration Beam"], obliteration_beam_timer, GetSoundSetting(true, "SoundObliterationBeam"))
+			core:AddBar("GGRID", self.L["~Gun Grid"], gungrid_timer, GetSoundSetting(true, "SoundGunGrid"))
 			if GetSetting("OtherHandSpawnMarkers") then
-				core:SetWorldMarker(handpos["hand1"], "Hand 1")
-				core:SetWorldMarker(handpos["hand2"], "Hand 2")
+				core:SetWorldMarker(handpos["hand1"], self.L["Hand %u"])
+				core:SetWorldMarker(handpos["hand2"], self.L["Hand %u"])
 			end
 			if GetSetting("OtherDirectionMarkers") then
-				core:SetWorldMarker(referencePos["north"], "North")
-				core:SetWorldMarker(referencePos["south"], "South")
+				core:SetWorldMarker(referencePos["north"], self.L["MARKER North"])
+				core:SetWorldMarker(referencePos["south"], self.L["MARKER South"])
 			end
 			gungrid_timer = 112
 			obliteration_beam_timer = 37
-		elseif sName == "Infinite Logic Loop" then
+		elseif sName == self.L["Infinite Logic Loop"] then
 			local strRedBuffs = "Red Buffs:"
 			local strGreenBuffs = "Green Buffs:"
 			local strBlueBuffs = "Blue Buffs:"
