1339a761a268a63178c688b8eebca7395e61ee62
Immé sama <immemor@users.noreply.github.com>
2015-04-06 20:16:11 +0200

    Modules: Add locale for EpEarthAir

--- Modules/EpEarthAir.lua
+++ Modules/EpEarthAir.lua
@@ -10,6 +10,34 @@ if not mod then return end
 --mod:RegisterEnableMob("Megalith")
 mod:RegisterEnableBossPair("Megalith", "Aileron")
 mod:RegisterRestrictZone("EpEarthAir", "Elemental Vortex Alpha", "Elemental Vortex Beta", "Elemental Vortex Delta")
+mod:RegisterEnglishLocale({
+	-- Unit names.
+	["Megalith"] = "Megalith",
+	["Aileron"] = "Aileron",
+	["Air Column"] = "Air Column",
+	-- Datachron messages.
+	["The ground shudders beneath Megalith"] = "The ground shudders beneath Megalith",
+	["fractured crust leaves it exposed"] = "fractured crust leaves it exposed",
+	-- Cast.
+	["Supercell"] = "Supercell",
+	["Raw Power"] = "Raw Power",
+	-- Bar and messages.
+	["MOO !"] = "MOO !",
+	["EARTH"] = "EARTH",
+	["~Tornado Spawn"] = "~Tornado Spawn",
+})
+mod:RegisterFrenchLocale({
+	-- Unit names.
+	-- Datachron messages.
+	-- Cast.
+	-- Bar and messages.
+})
+mod:RegisterGermanLocale({
+	-- Unit names.
+	-- Datachron messages.
+	-- Cast.
+	-- Bar and messages.
+})
 
 --------------------------------------------------------------------------------
 -- Locals
@@ -25,9 +53,9 @@ local startTime
 
 function mod:OnBossEnable()
 	Print(("Module %s loaded"):format(mod.ModuleName))
-	Apollo.RegisterEventHandler("UnitCreated", "OnUnitCreated", self)
-	Apollo.RegisterEventHandler("UnitDestroyed", "OnUnitDestroyed", self)
-	Apollo.RegisterEventHandler("UnitEnteredCombat", "OnCombatStateChanged", self)
+	Apollo.RegisterEventHandler("RC_UnitCreated", "OnUnitCreated", self)
+	Apollo.RegisterEventHandler("RC_UnitDestroyed", "OnUnitDestroyed", self)
+	Apollo.RegisterEventHandler("RC_UnitStateChanged", "OnUnitStateChanged", self)
 	Apollo.RegisterEventHandler("SPELL_CAST_START", "OnSpellCastStart", self)
 	Apollo.RegisterEventHandler("CHAT_DATACHRON", "OnChatDC", self)
 	--Apollo.RegisterEventHandler("DEBUFF_APPLIED", "OnDebuffApplied", self)
@@ -38,68 +66,66 @@ end
 -- Event Handlers
 --
 
-function mod:OnUnitCreated(unit)
-	local sName = unit:GetName()
+function mod:OnUnitCreated(unit, sName)
 	local eventTime = GameLib.GetGameTime()
 	--Print(sName)
-	if sName == "Air Column" then
+	if sName == self.L["Air Column"] then
 		core:AddLine(unit:GetId(), 2, unit, nil, 3, 30, 0, 10)
 		if eventTime > startTime + 10 then
 			core:StopBar("TORNADO")
-			core:AddBar("TORNADO", "~Tornado Spawn", 17, true)
+			core:AddBar("TORNADO", self.L["~Tornado Spawn"], 17, true)
 		end
 	end
 end
 
-function mod:OnUnitDestroyed(unit)
+function mod:OnUnitDestroyed(unit, sName)
 	local sName = unit:GetName()
 	--Print(sName)
-	if sName == "Air Column" then
+	if sName == self.L["Air Column"] then
 		core:DropLine(unit:GetId())
 	end
 end
 
 function mod:OnSpellCastStart(unitName, castName, unit)
-	if unitName == "Megalith" and castName == "Raw Power" then
+	if unitName == self.L["Megalith"] and castName == self.L["Raw Power"] then
 			midphase = true
-			core:AddMsg("RAW", "RAW POWER", 5, "Alert")
-	elseif unitName == "Aileron" and castName == "Supercell" then
+			core:AddMsg("RAW", self.L["Raw Power"]:upper(), 5, "Alert")
+	elseif unitName == self.L["Aileron"] and castName == self.L["Supercell"] then
 		local timeOfEvent = GameLib.GetGameTime()
 		if timeOfEvent - prev > 30 then
 			prev = timeOfEvent
-			core:AddMsg("CELL", "SUPERCELL", 5, "Alarm")
-			core:AddBar("CELL", "SUPERCELL", 80)
+			core:AddMsg("CELL", self.L["Supercell"]:upper(), 5, "Alarm")
+			core:AddBar("CELL", self.L["Supercell"]:upper(), 80)
 		end
 	end
 end
 
 function mod:OnChatDC(message)
-	if message:find("The ground shudders beneath Megalith") then
+	if message:find(self.L["The ground shudders beneath Megalith"]) then
 		core:AddMsg("QUAKE", "JUMP !", 3, "Beware")
-	elseif message:find("fractured crust leaves it exposed") and midphase then
+	elseif message:find(self.L["fractured crust leaves it exposed"]) and midphase then
 		midphase = false
-		core:AddMsg("MOO", "MOO !", 5, "Info", "Blue")
-		core:AddBar("RAW", "RAW POWER", 60, 1)
+		core:AddMsg("MOO", self.L["MOO !"], 5, "Info", "Blue")
+		core:AddBar("RAW", self.L["Raw Power"]:upper(), 60, 1)
 	end
 end
 
-function mod:OnCombatStateChanged(unit, bInCombat)
+function mod:OnUnitStateChanged(unit, bInCombat, sName)
 	if unit:GetType() == "NonPlayer" and bInCombat then
-		local sName = unit:GetName()
 		local eventTime = GameLib.GetGameTime()
 		startTime = eventTime
 
-		if sName == "Megalith" then
+		if sName == self.L["Megalith"] then
 			core:AddUnit(unit)
 			core:WatchUnit(unit)
-			core:MarkUnit(unit, nil, "EARTH")
-		elseif sName == "Aileron" then
+			core:MarkUnit(unit, nil, self.L["EARTH"])
+		elseif sName == self.L["Aileron"] then
 			self:Start()
 			prev = 0
 			midphase = false
 			core:AddPixie(unit:GetId(), 2, unit, nil, "Green", 10, 15, 0)
-			core:AddBar("SCELL", "Supercell", 65, 1)
-			core:AddBar("TORNADO", "~Tornado Spawn", 16, true)
+			core:AddBar("SCELL", self.L["Supercell"], 65, 1)
+			core:AddBar("TORNADO", self.L["~Tornado Spawn"], 16, true)
 			core:AddUnit(unit)
 			core:WatchUnit(unit)
 			core:StartScan()
