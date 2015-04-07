1627ec9d1f0d29140eaee74fdf58a8f715e31846
Immé sama <immemor@users.noreply.github.com>
2015-04-07 03:48:24 +0200

    Modules: Add locale for LogicWing

--- Modules/DSLogicWing.lua
+++ Modules/DSLogicWing.lua
@@ -8,6 +8,37 @@ local mod = core:NewBoss("DSLogicWing", 52)
 if not mod then return end
 
 mod:RegisterEnableMob("Hyper-Accelerated Skeledroid", "Augmented Herald of Avatus", "Abstract Augmentation Algorithm")
+mod:RegisterEnglishLocale({
+	-- Unit names.
+	["Conjured Fire Bomb"] = "Conjured Fire Bomb",
+	["Abstract Augmentation Algorithm"] = "Abstract Augmentation Algorithm",
+	["Augmented Herald of Avatus"] = "Augmented Herald of Avatus",
+	["Quantum Processing Unit"] = "Quantum Processing Unit",
+	["Hyper-Accelerated Skeledroid"] = "Hyper-Accelerated Skeledroid",
+	-- Datachron messages.
+	["The Abstract Augmentation Algorithm has amplified a Quantum Processing Unit"] = "The Abstract Augmentation Algorithm has amplified a Quantum Processing Unit",
+	-- Cast.
+	["Data Deconstruction"] = "Data Deconstruction",
+	-- Bar and messages.
+	["[%u] INTERRUPT"] = "[%u] INTERRUPT",
+	["HEAL SOON"] = "HEAL SOON",
+	["CUBE SMASH"] = "CUBE SMASH",
+	["EMPOWER"] = "EMPOWER%s",
+	["BOMB"] = "BOMB",
+	["BERSERK"] = "BERSERK",
+})
+mod:RegisterFrenchLocale({
+	-- Unit names.
+	-- Datachron messages.
+	-- Cast.
+	-- Bar and messages.
+})
+mod:RegisterGermanLocale({
+	-- Unit names.
+	-- Datachron messages.
+	-- Cast.
+	-- Bar and messages.
+})
 
 --------------------------------------------------------------------------------
 -- Locals
@@ -23,52 +54,49 @@ local nbKick = 28
 
 function mod:OnBossEnable()
 	Print(("Module %s loaded"):format(mod.ModuleName))
-	Apollo.RegisterEventHandler("UnitEnteredCombat", "OnCombatStateChanged", self)
+	Apollo.RegisterEventHandler("RC_UnitEnteredCombat", "OnUnitEnteredCombat", self)
 	Apollo.RegisterEventHandler("SPELL_CAST_START", "OnSpellCastStart", self)
 	Apollo.RegisterEventHandler("CHAT_DATACHRON", "OnChatDC", self)
 	Apollo.RegisterEventHandler("DEBUFF_APPLIED", "OnDebuffApplied", self)
 	Apollo.RegisterEventHandler("DEBUFF_APPLIED_DOSE", "OnDebuffApplied", self)
 	Apollo.RegisterEventHandler("UNIT_HEALTH", "OnHealthChanged", self)
-	--Apollo.RegisterEventHandler("UnitCreated", "OnUnitCreated", self)
+	--Apollo.RegisterEventHandler("RC_UnitCreated", "OnUnitCreated", self)
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
-function mod:OnUnitCreated(unit)
-	local sName = unit:GetName()
+function mod:OnUnitCreated(unit, sName)
 	--Print(sName)
-	if sName == "Conjured Fire Bomb" then
-		core:AddMsg("BOMB", "BOMB", 5, "Long", "Blue")
-		core:AddBar("BOMB", "BOMB", first and 20 or 23)
+	if sName == self.L["Conjured Fire Bomb"] then
+		core:AddMsg("BOMB", self.L["BOMB"], 5, "Long", "Blue")
+		core:AddBar("BOMB", self.L["BOMB"], first and 20 or 23)
 	end
 end
 
 
 function mod:OnHealthChanged(unitName, health)
-	if unitName == "Augmented Herald of Avatus" and health == 25 then
-		core:AddMsg("SIPHON", "HEAL SOON", 5, "Info")
+	if unitName == self.L["Augmented Herald of Avatus"] and health == 25 then
+		core:AddMsg("SIPHON", self.L["HEAL SOON"], 5, "Info")
 	end
 end
 
 function mod:OnSpellCastStart(unitName, castName, unit)
-	if unitName == "Augmented Herald of Avatus" and castName == "Cube Smash" then
-		core:AddBar("CUBE", "CUBE SMASH", 17)
-	elseif unitName == "Abstract Augmentation Algorithm" and castName == "Data Deconstruction" then
+	if unitName == self.L["Augmented Herald of Avatus"] and castName == "Cube Smash" then
+		core:AddBar("CUBE", self.L["CUBE SMASH"], 17)
+	elseif unitName == self.L["Abstract Augmentation Algorithm"] and castName == self.L["Data Deconstruction"] then
 		castCount = castCount + 1
-		core:AddMsg("DATA", ("[%s] INTERRUPT"):format(castCount), 3, "Long", "Blue")
-		core:AddBar("DATA", ("[%s] INTERRUPT"):format(castCount), 7)
+		core:AddMsg("DATA", self.L["[%u] INTERRUPT"]:format(castCount), 3, "Long", "Blue")
+		core:AddBar("DATA", self.L["[%u] INTERRUPT"]:format(castCount), 7)
 	end
 end
 
 function mod:OnChatDC(message)
-	if message:find("The Abstract Augmentation Algorithm has amplified a Quantum Processing Unit") then
-		core:AddMsg("EMPOWER", "EMPOWER !!", 5, "Alert")
-		core:AddBar("EMPOWER", "EMPOWER", 30, 1)
+	if message:find(self.L["The Abstract Augmentation Algorithm has amplified a Quantum Processing Unit"]) then
+		core:AddMsg("EMPOWER", self.L["EMPOWER"]:format(" !!"), 5, "Alert")
+		core:AddBar("EMPOWER", self.L["EMPOWER"]:format(""), 30, 1)
 	end
 end
-
-
 
 function mod:OnDebuffApplied(unitName, splId)
 	if splId == 72559 and prevInt ~= unitName then
@@ -79,16 +107,15 @@ function mod:OnDebuffApplied(unitName, splId)
 		prevInt = unitName
 		castCount = castCount + 1
 		if castCount > nbKick then castCount = 1 end
-		--core:AddMsg("DATA", ("[%s] INTERRUPT"):format(castCount), 3, "Long", "Blue")
-		core:AddBar("DATA", ("[%s] INTERRUPT"):format(castCount), 7)
+		--core:AddMsg("DATA", self.L["[%u] INTERRUPT"]:format(castCount), 3, "Long", "Blue")
+		core:AddBar("DATA", self.L["[%u] INTERRUPT"]:format(castCount), 7)
 		--end
 	end
 end
 
-function mod:OnCombatStateChanged(unit, bInCombat)
+function mod:OnUnitEnteredCombat(unit, bInCombat, sName)
 	if unit:GetType() == "NonPlayer" and bInCombat then
-		local sName = unit:GetName()
-		if sName == "Abstract Augmentation Algorithm" then
+		if sName == self.L["Abstract Augmentation Algorithm"] then
 			self:Start()
 			prevInt = ""
 			castCount = 1
@@ -96,22 +123,22 @@ function mod:OnCombatStateChanged(unit, bInCombat)
 			--core:WatchUnit(unit)
 			core:RaidDebuff()
 			core:StartScan()
-			core:AddBar("DATA", ("[%s] INTERRUPT"):format(castCount), 7)
-			core:AddBar("EMPOWER", "EMPOWER", 30, 1)
-		elseif sName == "Quantum Processing Unit" then
+			core:AddBar("DATA", self.L["[%u] INTERRUPT"]:format(castCount), 7)
+			core:AddBar("EMPOWER", self.L["EMPOWER"]:format(""), 30, 1)
+		elseif sName == self.L["Quantum Processing Unit"] then
 			self:Start()
 			core:AddUnit(unit)
 			core:MarkUnit(unit)
-		elseif sName == "Hyper-Accelerated Skeledroid" then
+		elseif sName == self.L["Hyper-Accelerated Skeledroid"] then
 			self:Start()
 			core:AddUnit(unit)
-			core:AddBar("BERSERK", "BERSERK", 180, 1)
-		elseif sName == "Augmented Herald of Avatus" then
+			core:AddBar("BERSERK", self.L["BERSERK"], 180, 1)
+		elseif sName == self.L["Augmented Herald of Avatus"] then
 			self:Start()
 			core:AddUnit(unit)
 			core:WatchUnit(unit)
 			core:StartScan()
-			core:AddBar("CUBE", "CUBE SMASH", 8)
+			core:AddBar("CUBE", self.L["CUBE SMASH"], 8)
 		end
 	end
 end
