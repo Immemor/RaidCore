ba0cb541e87ab21379a06dfb126d27e74b023507
Immé sama <immemor@users.noreply.github.com>
2015-05-24 02:02:40 +0200

    Update the version variable, for tag 3.0-alpha

--- RaidCore.lua
+++ RaidCore.lua
@@ -53,7 +53,7 @@ local _tCurrentEncounter = nil
 
 local trackMaster = Apollo.GetAddon("TrackMaster")
 local markCount = 0
-local AddonVersion = 15051101
+local AddonVersion = 15052401
 local VCReply, VCtimer = {}, nil
 local CommChannelTimer = nil
 local empCD, empTimer = 5, nil
