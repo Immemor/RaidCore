0a7f845cf92838586fe11302cf8daa9d030e0663
Immé sama <immemor@users.noreply.github.com>
2015-08-23 06:34:25 +0200

    Encounters/DS: Add circles on BEAX of Limbo Infomatrix, closes #116

--- Encounters/DS/LimboInfomatrix.lua
+++ Encounters/DS/LimboInfomatrix.lua
@@ -56,6 +56,13 @@ mod:RegisterDefaultSetting("OtherMarkerAntlion")
 local GetPlayerUnit = GameLib.GetPlayerUnit
 local GetGameTime = GameLib.GetGameTime
 
+-- Is the player is far from the Keeper Of Sands.
+-- @param tKeeperUnit  unit object which represent the Keeper of Sands.
+-- @return  true player is under the 40 meters, false otherwise.
+local function IsInRangeOfKeeper(tKeeperUnit)
+    return mod:GetDistanceBetweenUnits(GetPlayerUnit(), tKeeperUnit) < 40
+end
+
 ----------------------------------------------------------------------------------------------------
 -- Encounter description.
 ----------------------------------------------------------------------------------------------------
@@ -75,20 +82,30 @@ function mod:OnUnitCreated(unit, sName)
             core:MarkUnit(unit)
         end
     elseif sName == self.L["BEAX"] then
-        -- TODO: Draw 2 circles: 7.5 and 14 of radius.
-        -- core:SetWorldMarker(unit:GetId(), "x", unit:GetPosition())
+        if mod:GetSetting("LinePathOfInvisibleUnit") then
+            if IsInRangeOfKeeper(unit) then
+                local nId = unit:GetId()
+                local sKey1 = ("BEAX %d-%d"):format(nId, 1)
+                local sKey2 = ("BEAX %d-%d"):format(nId, 2)
+                core:AddPolygon(sKey1, nId, 8, 0, 3, "xkcdBrightPurple", 12)
+                core:AddPolygon(sKey1, nId, 15, 0, 3, "xkcdBluishPurple", 12)
+            end
+        end
     end
 end
 
 function mod:OnUnitDestroyed(unit, sName)
     if sName == self.L["BEAX"] then
-        -- core:DropWorldMarker(unit:GetId())
+        local nId = unit:GetId()
+        for i=1, 2 do
+            core:RemovePolygon(("BEAX %d-%d"):format(nId, i))
+        end
     end
 end
 
 function mod:OnSpellCastStart(unitName, castName, unit)
     if unitName == self.L["Keeper of Sands"] then
-        local bInRange = mod:GetDistanceBetweenUnits(GetPlayerUnit(), unit) < 40
+        local bInRange = IsInRangeOfKeeper(unit)
         if bInRange then
             if castName == self.L["Desiccate"] then
                 if mod:GetSetting("SoundDessicateInterrupt") then
