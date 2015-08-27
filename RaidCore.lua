72c0b5a536f990e4e864aaece2ce878d88566433
Immé sama <immemor@users.noreply.github.com>
2015-08-27 23:23:24 +0200

    Update version string

--- RaidCore.lua
+++ RaidCore.lua
@@ -27,9 +27,9 @@ local GetCurrentZoneMap = GameLib.GetCurrentZoneMap
 -- Constants.
 ----------------------------------------------------------------------------------------------------
 -- Should be @project-version@ when replacement tokens will works (see #88 issue).
-local RAIDCORE_CURRENT_VERSION = "4.4-alpha"
+local RAIDCORE_CURRENT_VERSION = "4.5-alpha"
 -- Should be deleted.
-local ADDON_DATE_VERSION = 15082701
+local ADDON_DATE_VERSION = 15082702
 -- Sometimes Carbine have inserted some no-break-space, for fun.
 -- Behavior seen with French language. This problem is not present in English.
 local NO_BREAK_SPACE = string.char(194, 160)
