d1063aaad1d1aaac85f3badbbdb2b8e73b9f2ecc
Immé sama <immemor@users.noreply.github.com>
2015-10-22 19:24:57 +0200

    Update version string

--- RaidCore.lua
+++ RaidCore.lua
@@ -29,9 +29,9 @@ local next, pcall  = next, pcall
 -- Constants.
 ----------------------------------------------------------------------------------------------------
 -- Should be @project-version@ when replacement tokens will works (see #88 issue).
-local RAIDCORE_CURRENT_VERSION = "5.8"
+local RAIDCORE_CURRENT_VERSION = "5.9"
 -- Should be deleted.
-local ADDON_DATE_VERSION = 15102001
+local ADDON_DATE_VERSION = 15102201
 -- Sometimes Carbine have inserted some no-break-space, for fun.
 -- Behavior seen with French language. This problem is not present in English.
 local NO_BREAK_SPACE = string.char(194, 160)
