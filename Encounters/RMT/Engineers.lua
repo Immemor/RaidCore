d63cb81dd99c8770f688831539a65a072a5d9b0b
Jeffroiscool <jeffroiscool@gmail.com>
2016-09-02 12:04:16 +0200

    Added Electroshock debuff. Added Electroshock datachron message. Added message for Liquidate stack. Added message when Electroshock is being cast. Added suspected timer values as locals for Electroshock.

--- Encounters/RMT/Engineers.lua
+++ Encounters/RMT/Engineers.lua
@@ -27,15 +27,18 @@ mod:RegisterEnglishLocale({
     ["Cooling Turbine"] = "Cooling Turbine",
     ["Spark Plug"] = "Spark Plug",
     ["Lubricant Nozzle"] = "Lubricant Nozzle",
+	-- Engineer
     ["Head Engineer Orvulgh"] = "Head Engineer Orvulgh",
+	-- Warrior
     ["Chief Engineer Wilbargh"] = "Chief Engineer Wilbargh",
     ["Air Current"] = "Air Current",
     ["Friendly Invisible Unit for Fields"] = "Friendly Invisible Unit for Fields",
     ["Hostile Invisible Unit for Fields (0 hit radius)"] = "Hostile Invisible Unit for Fields (0 hit radius)",
     -- Cast names
     ["Liquidate"] = "Liquidate",
+	["Electroshock"] = "Electroshock",
     -- Datachron
-    ["Hornious Eversong suffers from Electroshock"] = "",
+    ["suffers from Electroshock"] = "suffers from Electroshock",
   })
 ----------------------------------------------------------------------------------------------------
 -- Constants.
@@ -47,6 +50,13 @@ mod:RegisterEnglishLocale({
 local GetUnitById = GameLib.GetUnitById
 local GetPlayerUnit = GameLib.GetPlayerUnit
 local GetGameTime = GameLib.GetGameTime
+
+local DEBUFF_ION_CLASH = 84051
+local DEBUFF_UNSTABLE_VOLTAGE = 84045
+
+local FIRST_ELECTROSHOCK_TIMER = 12
+local ELECTROSHOCK_TIMER = 20
+
 ----------------------------------------------------------------------------------------------------
 -- Encounter description.
 ----------------------------------------------------------------------------------------------------
@@ -61,7 +71,19 @@ function mod:OnCastStart(nId, sCastName, nCastEndTime, sName)
 end
 
 function mod:OnDebuffAdd(nId, nSpellId, nStack, fTimeRemaining)
+  if DEBUFF_ION_CLASH == nSpellId then
+    mod:AddMsg("ION_CLASH_MSG", "KITE THE ORB", 5, "RunAway")
+	core:AddPicture(nId, nId, "Crosshair", 20)
+  end
+  if DEBUFF_UNSTABLE_VOLTAGE == nSpellId then
+    mod:AddMsg("UNSTABLE_VOLTAGE_MSG", "GET AWAY FROM THE CENTER", 5, "RunAway")
+  end
+end
 
+function mod:OnDebuffRemove(nId, nSpellId, nStack, fTimeRemaining)
+  if nSpellId == DEBUFF_ION_CLASH then
+    core:RemovePicture(nId)
+  end
 end
 
 function mod:OnUnitDestroyed(nId, tUnit, sName)
@@ -85,13 +107,42 @@ mod:RegisterUnitEvents({
     end,
   }
 )
+
+-- Warrior
 mod:RegisterUnitEvents("Chief Engineer Wilbargh",{
     ["OnCastStart"] = function (self, nId, sCastName, nCastEndTime, sName)
       if self.L["Liquidate"] == sCastName then
         --Stack
+		mod:AddMsg("LIQUIDATE", "Stack", 5, "Info")
       end
     end,
   }
+)
+
+-- Engineer
+mod:RegisterUnitEvents("Head Engineer Orvulgh",{
+    ["OnCastStart"] = function (self, nId, sCastName, nCastEndTime, sName)
+      if self.L["Electroshock"] == sCastName then
+		mod:AddMsg("Electroshock", "Electroshock", 5, "Info")
+		
+      end
+    end,
+  }
+)
+
+mod:RegisterDatachronEvent("suffers from Electroshock", "FIND", function (self, sMessage)
+    local tElectroshockTarget = GetPlayerUnitByName(string.match(sMessage, "([^%s]+%s[^%s]+)".." "..self.L["suffers from Electroshock"]))
+    local bIsOnMyself = tElectroshockTarget == playerUnit
+    local sSound = "Info"
+    local sElectroshockOnX = ""
+    if bIsOnMyself then
+      sElectroshockOnX = self.L["YOU SWAP TO WARRIOR"]
+    else
+      sElectroshockOnX = self.L["%s SWAP TO WARRIOR"]:format(tElectroshockTarget:GetName())
+    end
+
+    mod:AddMsg("ELECTROSHOCK_MSG", sElectroshockOnX, 5, sSound, "Red")
+  end
 )
 
 -- mod:RegisterUnitEvents({"Friendly Invisible Unit for Fields"},{
