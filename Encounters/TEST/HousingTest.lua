c480ce81d211300b0e8da185492b31d6ec8a327d
Julian Hangstörfer <jhangstoerfer@gmail.com>
2016-09-13 23:27:51 +0200

    Init test encounter with unit names and casts

---
+++ Encounters/TEST/HousingTest.lua
@@ -0,0 +1,116 @@
+----------------------------------------------------------------------------------------------------
+-- Client Lua Script for RaidCore Addon on WildStar Game.
+--
+-- Copyright (C) 2015 RaidCore
+----------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+-- Description:
+-- Fake boss, to test few basic feature in RaidCore.
+--
+-- This last should be declared only in alpha version or with git database.
+----------------------------------------------------------------------------------------------------
+local core = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:GetAddon("RaidCore")
+--@alpha@
+local mod = core:NewEncounter("HousingTest", 36, 0, 60)
+--@end-alpha@
+if not mod then return end
+
+----------------------------------------------------------------------------------------------------
+-- Registering combat.
+----------------------------------------------------------------------------------------------------
+local ALL_MOBS = {
+  -- Easy.
+  "unit.chompacabra",
+  "unit.moodie",
+  "unit.aggressorbot",
+  -- Normal.
+  "unit.bandit",
+  "unit.girrok",
+  "unit.shootbot",
+  -- Hard.
+  "unit.pumera",
+  "unit.osun",
+  "unit.krogg",
+  -- Elite.
+  "unit.ravenok",
+  "unit.pell",
+  "unit.titan",
+}
+
+mod:RegisterTrigMob("ANY", ALL_MOBS)
+mod:RegisterEnglishLocale({
+    -- Unit names.
+    -- Easy.
+    ["unit.chompacabra"] = "Holographic Chompacabra",
+    ["unit.moodie"] = "Holographic Moodie",
+    ["unit.aggressorbot"] = "Holographic Aggressorbot",
+    -- Normal.
+    ["unit.bandit"] = "Holographic Bandit",
+    ["unit.girrok"] = "Holographic Girrok",
+    ["unit.shootbot"] = "Holographic Shootbot",
+    -- Hard.
+    ["unit.pumera"] = "Holographic Pumera",
+    ["unit.osun"] = "Holographic Osun",
+    ["unit.krogg"] = "Holographic Krogg",
+    -- Elite.
+    ["unit.ravenok"] = "Holographic Ravenok",
+    ["unit.pell"] = "Holographic Pell",
+    ["unit.titan"] = "Holographic Titan",
+    ["unit.buffer"] = "Invisible Unit s71975 - Aquatic Buffer", -- titan cast,
+
+    ["unit.invis.0"] = "Hostile Invisible Unit for Fields (0 hit radius)",
+    -- Cast names.
+    ["cast.chompacabra.frenzy"] = "Feeding Frenzy",
+    ["cast.chompacabra.trap"] = "Snap Trap",
+    ["cast.moodie.firestorm"] = "Firestorm",
+    ["cast.moodie.fissure"] = "Erupting Fissure",
+    ["cast.shootbot.dash"] = "Slasher Dash",
+    ["cast.shootbot.jump"] = "Jump Shot",
+
+    ["unit.bandit.blast"] = "Blast Shot",
+    ["unit.girrok.roar"] = "Roar",
+    ["unit.aggressorbot.spin"] = "Mauling Spin",
+
+    ["unit.pumera.pounce"] = "Pride Pounce",
+    ["unit.pumera.frenzy"] = "Frenzy of the Feline",
+    ["unit.osun.firestorm"] = "Firestorm",
+    ["unit.osun.fisure"] = "Erupting Fissure",
+    ["unit.krogg.rush"] = "Crushing Rush",
+    ["unit.krogg.flurry"] = "Crushing Flurry",
+    ["unit.krogg.assault"] = "Focused Assault",
+
+    ["unit.ravenok.head_shoulders"] = "Head and Shoulders",
+    ["unit.ravenok.tooth_nail"] = "Tooth and Nail",
+    ["unit.ravenok.winds"] = "Tail Winds",
+    ["unit.ravenok.rush"] = "Ravenok Rush",
+    ["unit.titan.slash"] = "Shellark Slash",
+    ["unit.titan.strike"] = "Trident Strike",
+    ["unit.titan.buffer"] = "Aquatic Buffer",
+    ["unit.pell.vortex"] = "Swirling Vortex",
+  })
+
+----------------------------------------------------------------------------------------------------
+-- Constants.
+----------------------------------------------------------------------------------------------------
+local BUFF_KROGG_FOCUSED_ASSAULT = 72202
+--moodie and chompacabra debuff
+local DEBUFF_MELT = 50233
+local DEBUFF_CHOMPACABRA_BLEED = 43759
+----------------------------------------------------------------------------------------------------
+-- Locals.
+----------------------------------------------------------------------------------------------------
+
+----------------------------------------------------------------------------------------------------
+-- Encounter description.
+----------------------------------------------------------------------------------------------------
+function mod:OnBossEnable()
+
+end
+
+mod:RegisterUnitEvents(ALL_MOBS,{
+    ["OnUnitCreated"] = function (_, _, unit)
+      core:WatchUnit(unit)
+      core:AddUnit(unit)
+    end,
+  }
+)
