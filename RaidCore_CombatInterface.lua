00cc399a20e6d9fb488d29b4af635e192b503413
Julian Hangstörfer <jhangstoerfer@gmail.com>
2016-09-18 22:48:16 +0200

    Remove unused local GetPlayerUnitByName

--- RaidCore_CombatInterface.lua
+++ RaidCore_CombatInterface.lua
@@ -28,7 +28,6 @@ local Log = Apollo.GetPackage("Log-1.0").tPackage
 ----------------------------------------------------------------------------------------------------
 local RegisterEventHandler = Apollo.RegisterEventHandler
 local RemoveEventHandler = Apollo.RemoveEventHandler
-local GetPlayerUnitByName = GameLib.GetPlayerUnitByName
 local GetGameTime = GameLib.GetGameTime
 local GetPlayerUnit = GameLib.GetPlayerUnit
 local GetUnitById = GameLib.GetUnitById
