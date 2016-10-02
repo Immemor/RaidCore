d66cd59e85351f3a51288f737bb59036397e4333
Julian Hangstörfer <jhangstoerfer@gmail.com>
2016-10-03 01:55:11 +0200

    Add some german translations for buttons

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -8,5 +8,10 @@ local GeminiLocale = Apollo.GetPackage("Gemini:Locale-1.0").tPackage
 local L = GeminiLocale:NewLocale("RaidCore", "deDE", false, true)
 if not L then return end
 
+L["Start test scenario"] = "Testszenario starten"
+L["Stop test scenario"] = "Testszenario stoppen"
+L["Move Bars"] = "Balken bewegen"
+L["Lock Bars"] = "Balken sperren"
+L["Reset Bars"] = "Balken resetten"
 L["csi.summon"] = "Zu deinem Gruppenmitglied teleportieren?"
 L["message.summon.request"] = "%s versucht dich herbeizurufen. Versuche jetzt anzunehmen."
