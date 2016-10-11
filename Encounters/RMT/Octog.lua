e67b89a6cd38dfe7ff1defa9a7379bb9e8387285
Jeffroiscool <jeffroiscool@gmail.com>
2016-10-11 23:45:35 +0200

    Timers

--- Encounters/RMT/Octog.lua
+++ Encounters/RMT/Octog.lua
@@ -22,7 +22,15 @@ mod:RegisterEnglishLocale({
     ["unit.orb"] = "Chaos Orb",
     ["unit.pool"] = "Noxious Ink Pool",
     -- Cast names.
+    ["cast.supernova"] = "Supernova",
+    ["cast.hookshot"] = "Hookshot",
+    ["cast.flamethrower"] = "Flamethrower",
     -- Messages.
+    ["msg.hookshot.next"] = "Next hookshot in",
+    ["msg.flamethrower.next"] = "Next flamethrower in",
+    ["msg.chaos.orbs.coming"] = "Chaos orb(s) coming soon",
+    ["msg.midphase.coming"] = "Midphase coming soon",
+    ["msg.midphase.started"] = "MIDPHASE",
   })
 ----------------------------------------------------------------------------------------------------
 -- Settings.
@@ -31,6 +39,21 @@ mod:RegisterEnglishLocale({
 -- Constants.
 ----------------------------------------------------------------------------------------------------
 
+-- Timers.
+local FIRST_HOOKSHOT_TIMER = 10
+local HOOKSHOT_TIMER = 45
+local FLAMETHROWER_TIMER = 40
+
+-- Health trackers
+local FIRST_CHAOS_ORB_UPPER_HEALTH = 86.5
+local FIRST_CHAOS_ORB_LOWER_HEALTH = 85.5
+
+local SECOND_CHAOS_ORB_UPPER_HEALTH = 71.5
+local SECOND_CHAOS_ORB_LOWER_HEALTH = 70.5
+
+local MIDPHASE_UPPER_HEALTH = 66.5
+local MIDPHASE_LOWER_HEALTH = 65.5
+
 ----------------------------------------------------------------------------------------------------
 -- Locals.
 ----------------------------------------------------------------------------------------------------
@@ -38,14 +61,51 @@ mod:RegisterEnglishLocale({
 ----------------------------------------------------------------------------------------------------
 -- Encounter description.
 ----------------------------------------------------------------------------------------------------
+function mod:OnBossEnable()
+  mod:AddTimerBar("NEXT_HOOKSHOT_TIMER", "msg.hookshot.next", FIRST_HOOKSHOT_TIMER)
+  mod:AddTimerBar("NEXT_FLAMETHROWER_TIMER", "msg.flamethrower.next", FLAMETHROWER_TIMER)
+  mod:DrawCompactorGrid()
+end
+
 mod:RegisterUnitEvents({
-    "unit.octog",
     "unit.orb",
     },{
     [core.E.UNIT_CREATED] = function (_, _, unit)
       core:AddUnit(unit)
       core:WatchUnit(unit)
     end,
+  }
+)
+
+mod:RegisterUnitEvents({
+    "unit.octog",
+    [core.E.UNIT_CREATED] = function (_, _, unit)
+      core:AddUnit(unit)
+      core:WatchUnit(unit)
+    end,
+    [core.E.HEALTH_CHANGED] = function(_, _, percent)
+      if (percent >= FIRST_CHAOS_ORB_LOWER_HEALTH and percent <= FIRST_CHAOS_ORB_UPPER_HEALTH) or (percent >= SECOND_CHAOS_ORB_LOWER_HEALTH and percent <= SECOND_CHAOS_ORB_UPPER_HEALTH) then
+        mod:AddMsg("CHAOS_ORB_SOON", "msg.chaos.orbs.coming", 5, "Info", "xkcdWhite")
+      end
+      if (percent >= MIDPHASE_LOWER_HEALTH and percent <= MIDPHASE_UPPER_HEALTH) then
+        mod:AddMsg("MIDPHASE_SOON", "msg.midphase.coming", 5, "Info", "xkcdWhite")
+      end
+    end,
+    [core.E.CAST_START] = {
+      ["cast.supernova"] = function(self)
+        mod:AddMsg("MIDPHASE_STARTED", "msg.midphase.started", 5, "Info", "xkcdWhite")
+        mod:RemoveTimerBar("NEXT_HOOKSHOT_TIMER")
+        mod:RemoveTimerBar("NEXT_FLAMETHROWER_TIMER")
+      end,
+    },
+    [core.E.CAST_END] = {
+      ["cast.hookshot"] = function(self)
+        mod:AddTimerBar("NEXT_HOOKSHOT_TIMER", "msg.hookshot.next", HOOKSHOT_TIMER)
+      end,
+      ["cast.flamethrower"] = function(self)
+        mod:AddTimerBar("NEXT_HOOKSHOT_TIMER", "msg.flamethrower.next", FLAMETHROWER_TIMER)
+      end
+    },
   }
 )
 
