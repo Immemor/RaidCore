228829844e4053bc52e0d558ac9e6495c7c6d42e
Julian Hangstörfer <jhangstoerfer@gmail.com>
2016-10-18 21:43:02 +0200

    Initiate Cropper miniboss encounter

---
+++ Encounters/RMT/miniboss/Cropper.lua
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------------------------
+-- Client Lua Script for RaidCore Addon on WildStar Game.
+--
+-- Copyright (C) 2015 RaidCore
+----------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+-- Description:
+-- TODO
+----------------------------------------------------------------------------------------------------
+local core = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:GetAddon("RaidCore")
+local mod = core:NewEncounter("Cropper", 104, 548, 553)
+if not mod then return end
+
+----------------------------------------------------------------------------------------------------
+-- Registering combat.
+----------------------------------------------------------------------------------------------------
+mod:RegisterTrigMob("ALL", { "unit.cropper" })
+mod:RegisterEnglishLocale({
+    -- Unit names.
+    ["unit.cropper"] = "Cropper Gho'Deke",
+    ["unit.rootbrute"] = "Rotten Rootbrute",
+    ["unit.stumpkin"] = "Scorching Stumpkin",
+
+    -- Cast names.
+    -- Messages.
+  })
+----------------------------------------------------------------------------------------------------
+-- Settings.
+----------------------------------------------------------------------------------------------------
+
+----------------------------------------------------------------------------------------------------
+-- Encounter description.
+----------------------------------------------------------------------------------------------------
+mod:RegisterUnitEvents({
+    "unit.cropper",
+    "unit.rootbrute",
+    "unit.stumpkin",
+    },{
+    [core.E.UNIT_CREATED] = function (_, _, unit)
+      core:AddUnit(unit)
+      core:WatchUnit(unit, core.E.TRACK_ALL)
+    end,
+  }
+)
