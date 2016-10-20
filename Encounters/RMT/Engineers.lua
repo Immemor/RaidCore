4dcc344c12f8378470ba9511001c836648ad7fa6
Julian Hangstörfer <jhangstoerfer@gmail.com>
2016-10-20 22:27:48 +0200

    Init french engineers translation

--- Encounters/RMT/Engineers.lua
+++ Encounters/RMT/Engineers.lua
@@ -78,6 +78,32 @@ mod:RegisterGermanLocale({
     ["msg.rocket_jump.moved"] = "%s MOVED",
   }
 )
+mod:RegisterFrenchLocale({
+    -- Unit names.
+    ["unit.fire_orb"] = "Plasma déchargé",
+    -- Public Event Names.
+    ["pub.fusion_core"] = "Noyau de fusion",
+    ["pub.cooling_turbine"] = "Turbine de refroidissement",
+    ["pub.spark_plug"] = "Bougie d'allumage",
+    ["pub.lubricant_nozzle"] = "Embout de lubrification",
+    -- Cast names.
+    -- Messages.
+    ["msg.warrior.liquidate.next"] = "Next Liquidate in",
+    ["msg.warrior.liquidate.stack"] = "Stack",
+    ["msg.engineer.electroshock.next"] = "Next Electroshock in",
+    ["msg.engineer.electroshock.swap.other"] = "%s SWAP TO WARRIOR",
+    ["msg.engineer.electroshock.swap.you"] = "YOU SWAP TO WARRIOR",
+    ["msg.engineer.electroshock.swap.return"] = "SWAP TO ENGINEER",
+    ["msg.fire_orb.next"] = "Next Fire Orb in",
+    ["msg.fire_orb.you"] = "FIRE ORB ON YOU",
+    ["msg.fire_orb.spawned"] = "Fire Orb",
+    ["msg.fire_orb.pop.timer"] = "Fire Orb is safe to pop in",
+    ["msg.fire_orb.pop.msg"] = "Pop the Orb",
+    ["msg.core.health.high.warning"] = "%s HIGH HEALTH!",
+    ["msg.core.health.low.warning"] = "%s LOW HEALTH!",
+    ["msg.rocket_jump.moved"] = "%s MOVED",
+  }
+)
 ----------------------------------------------------------------------------------------------------
 -- Constants.
 ----------------------------------------------------------------------------------------------------
