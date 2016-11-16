13d0c5d26d9f2109b137d7e530269b676dd79181
Julian Hangstörfer <jhangstoerfer@gmail.com>
2016-11-16 15:59:37 +0100

    Add speedup timer for sick beats

--- Encounters/RMT/miniboss/Kino.lua
+++ Encounters/RMT/miniboss/Kino.lua
@@ -21,14 +21,53 @@ mod:RegisterEnglishLocale({
     ["unit.station"] = "DJ Station",
     -- Cast names.
     -- Messages.
+    ["msg.kino.sick_beats.timer"] = "Sick beats interval %ds",
+    ["msg.kino.sick_beats.speedup"] = "Sick beats every %ds",
   })
 ----------------------------------------------------------------------------------------------------
 -- Settings.
 ----------------------------------------------------------------------------------------------------
-
+mod:RegisterDefaultTimerBarConfigs({
+    ["NEXT_SICK_BEATS_SPEEDUP"] = {sColor = "xkcdRed"},
+  }
+)
+----------------------------------------------------------------------------------------------------
+-- Constants.
+----------------------------------------------------------------------------------------------------
+local TIMERS = {
+  SICK_BEATS = {
+    SPEEDUP = 40,
+    START = 5.5,
+  }
+}
+----------------------------------------------------------------------------------------------------
+-- Locals.
+----------------------------------------------------------------------------------------------------
+local sickBeatsInterval
 ----------------------------------------------------------------------------------------------------
 -- Encounter description.
 ----------------------------------------------------------------------------------------------------
+function mod:OnBossEnable()
+  sickBeatsInterval = TIMERS.SICK_BEATS.START
+  mod:StartSpeedupTimer()
+end
+
+function mod:StartSpeedupTimer()
+  local msg = self.L["msg.kino.sick_beats.timer"]:format(sickBeatsInterval)
+  mod:AddTimerBar("NEXT_SICK_BEATS_SPEEDUP", msg, TIMERS.SICK_BEATS.SPEEDUP, nil, nil, mod.OnSpeedupTimer, mod)
+end
+
+function mod:ShowSpeedupMessage()
+  local msg = self.L["msg.kino.sick_beats.speedup"]:format(sickBeatsInterval)
+  mod:AddMsg("SICK_BEATS_SPEEDUP", msg, 5, "Info", "xkcdOrange")
+end
+
+function mod:OnSpeedupTimer()
+  sickBeatsInterval = sickBeatsInterval - 1
+  mod:ShowSpeedupMessage()
+  mod:StartSpeedupTimer()
+end
+
 mod:RegisterUnitEvents({"unit.kino", "unit.station"},{
     [core.E.UNIT_CREATED] = function (_, _, unit)
       core:AddUnit(unit)
