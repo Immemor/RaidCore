6cf0ce9e9c48025f468fc8ae19dd8f5b2628d7e2
Julian Hangstörfer <jhangstoerfer@gmail.com>
2016-11-16 16:40:00 +0100

    v6.3.0-beta

--- RaidCore.lua
+++ RaidCore.lua
@@ -28,9 +28,9 @@ local next, pcall = next, pcall
 -- Constants.
 ----------------------------------------------------------------------------------------------------
 -- Should be 5.23 when replacement tokens will works (see #88 issue).
-local RAIDCORE_CURRENT_VERSION = "6.2.9"
+local RAIDCORE_CURRENT_VERSION = "6.3.0-beta"
 -- Should be deleted.
-local ADDON_DATE_VERSION = 16110913
+local ADDON_DATE_VERSION = 16111619
 
 -- State Machine.
 local MAIN_FSM__SEARCH = 1
