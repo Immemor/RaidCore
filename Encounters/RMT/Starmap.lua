5a8383394c07028b5c552d85b21fca75feca474e
Julian Hangstörfer <jhangstoerfer@gmail.com>
2016-11-23 13:45:55 +0100

    Add several unit names and debuff ids

--- Encounters/RMT/Starmap.lua
+++ Encounters/RMT/Starmap.lua
@@ -14,18 +14,43 @@ if not mod then return end
 ----------------------------------------------------------------------------------------------------
 -- Registering combat.
 ----------------------------------------------------------------------------------------------------
-mod:RegisterTrigMob(core.E.TRIGGER_ALL, { "unit.alpha" })
+mod:RegisterTrigMob(core.E.TRIGGER_ANY, {
+    "unit.alpha",
+    "unit.aldinari",
+    "unit.cassus",
+    "unit.vulpes_nix",
+  }
+)
 mod:RegisterEnglishLocale({
     -- Unit names.
     ["unit.alpha"] = "Alpha Cassus",
+    ["unit.aldinari"] = "Aldinari",
+    ["unit.cassus"] = "Cassus",
+    ["unit.vulpes_nix"] = "Vulpes Nix",
+
+    ["unit.asteroid"] = "Rogue Asteroid",
+    ["unit.debris"] = "Cosmic Debris",
+    ["unit.debris_field"] = "Debris Field",
+    ["unit.pulsar"] = "Pulsar",
+    ["unit.world_ender"] = "World Ender",
     -- Cast names.
+    ["unit.alpha.flare"] = "Solar Flare",
+    -- Datachron.
+    ["unit.world_ender.aldinari"] = "A World Ender is heading to the Aldinari's orbit.",
+    ["unit.world_ender.vulpes_nix"] = "A World Ender is heading to the Vulpes Nix's orbit.",
     -- Messages.
   }
 )
 ----------------------------------------------------------------------------------------------------
--- Settings.
+-- Constants.
 ----------------------------------------------------------------------------------------------------
-
+local DEBUFFS = {
+  SOLAR_WINDS = 87536,
+  IRRADIATED_ARMOR = 84305,
+  ACCUMULATING_MASS = 84344,
+  PULSAR = 87542,
+  BURNING_ATMOSPHERE = 84301,
+}
 ----------------------------------------------------------------------------------------------------
 -- Encounter description.
 ----------------------------------------------------------------------------------------------------
