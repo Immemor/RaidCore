88b72812d5d6a91d9e3d128ca389431a4dc56fa5
Jeffroiscool <jeffroiscool@gmail.com>
2016-11-29 22:40:36 +0100

    Line to World Ender Warning for 6 stacks.

--- Encounters/RMT/Starmap.lua
+++ Encounters/RMT/Starmap.lua
@@ -41,6 +41,7 @@ mod:RegisterEnglishLocale({
     -- Messages.
     ["msg.asteroid.next"] = "Asteroids in",
     ["msg.world_ender.next"] = "World Ender in",
+    ["msg.solar.stacks"] = "%d STACKS",
   }
 )
 ----------------------------------------------------------------------------------------------------
@@ -87,7 +88,7 @@ function mod:OnAlphaCassusCreated(id, unit)
 end
 
 function mod:OnAsteroidCreated(id, unit)
-  core:AddLineBetweenUnits("ASTEROID_LINE_%s" .. id, playerId, id, 6, "red")
+  core:AddLineBetweenUnits("ASTEROID_LINE_%s" .. id, playerId, id, 4, "Blue")
   asteroidCount = asteroidCount + 1
   if asteroidCount >= 4 then
     asteroidCount = 0
@@ -109,10 +110,12 @@ function mod:OnWorldEnderCreated(id, unit)
   core:AddUnit(unit)
   asteroidClusterCount = 0
   mod:AddTimerBar("NEXT_WORLD_ENDER_TIMER", "msg.world_ender.next", TIMERS.WORLD_ENDER.NORMAL)
+  core:AddLineBetweenUnits("WORLD_ENDER_%s" .. id, playerId, id, 6, "Red")
 end
 
 function mod:OnWorldEnderDestroyed(id, unit)
   asteroidCount = asteroidCount - 1
+  core:RemoveLineBetweenUnits("WORLD_ENDER_%s" .. id)
 end
 
 function mod:OnDebrisCreated(id, unit)
@@ -122,6 +125,17 @@ end
 function mod:OnDebrisDestroyed(id, unit)
   core:RemovePicture("DEBRIS_PICTURE_" .. id)
 end
+
+mod:RegisterUnitEvents(core.E.ALL_UNITS, {
+    [DEBUFFS.SOLAR_WINDS] = {
+      [core.E.DEBUFF_UPDATE] = function(self, id, _, stack)
+        if playerId == id and stack == 6 then
+          mod:AddMsg("SOLAR_WINDS_MSG", string.format(self.L["msg.solar.stacks"], stack), 5, "Beware", "xkcdBloodOrange")
+        end
+      end
+    },
+  }
+)
 
 mod:RegisterUnitEvents("unit.alpha",{
     [core.E.UNIT_CREATED] = mod.OnAlphaCassusCreated,
