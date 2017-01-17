cd75370897dbbfe3f9da3461cb0884528660ac96
Julian Hangstörfer <jhangstoerfer@gmail.com>
2017-01-17 21:27:34 +0100

    Don't use a table for the center

--- Encounters/RMT/Laveka.lua
+++ Encounters/RMT/Laveka.lua
@@ -104,7 +104,7 @@ local TIMERS = {
   }
 }
 
-local ROOM_CENTER = Vector3.New({ x = -723.717773, y = 186.834915, z = -265.187195 })
+local ROOM_CENTER = Vector3.New(-723.717773, 186.834915, -265.187195)
 
 local SOUL_EATER_ORBITS = {
   [1] = Vector3.New(0, 0, 6),
